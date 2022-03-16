32c1e3fc3719ac7ee40f8d80218c7bcedcd81065
Justin <justinor340@gmail.com>
2022-03-16 19:26:47 -0400

    Update on program: shows runtime difference between recurrsion and iteration

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,5 +1,6 @@
 
 public class Fibonacci {
+	
 	public static int recurse(int n) 
 	{
 		if (n <= 1)
@@ -24,21 +25,53 @@ public class Fibonacci {
 		return fib1;
 			
 	}
+
+	public void Recurtest() {
+		
+		int[] arr = new int[] {2, 5, 9, 15};
+		long start = System.nanoTime();
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Fibonacci.recurse(arr[i]);
+			System.out.print(arr[i] + ", ");
+		}
+		long end = System.nanoTime();
+		System.out.print("Runtime in NanoSecond: " + (end - start));
+		System.out.println();
+	}
+	
+	public void Itertest() {
+		
+		long start1 = System.nanoTime();
+		System.out.print("Input Number: " + 2 +"\tFibonacci Number: " + Fibonacci.recurse(2));
+		long end1 = System.nanoTime();
+		long run1 = end1 - start1;
+		System.out.print("\tRuntime in Nanoseconds: " + run1);
+		System.out.println();
+		
+		long start2 = System.nanoTime();
+		System.out.print("Input Number: " + 5 +"\tFibonacci Number: " + Fibonacci.recurse(5));
+		long end2 = System.nanoTime();
+		long run2 = end2 - start2;
+		System.out.print("\tRuntime in Nanoseconds: " + run2);
+		System.out.println();
+		
+//		int[] arr = new int[] {2, 5, 9, 15};
+//		long start = System.nanoTime();
+//		for (int i = 0; i < arr.length; i++) {
+//			arr[i] = Fibonacci.iterate(arr[i]);
+//			System.out.print(arr[i] + ", ");
+//		}
+//		long end = System.nanoTime();
+//		System.out.print("Runtime in NanoSecond: " + (end - start));
+//		System.out.println();
+	}
 	
 	public static void main(String[] args) 
 	{
-		System.out.println("Recursive");
-		System.out.println(recurse(9));
-		System.out.println(recurse(15));
-		System.out.println(recurse(2));
-		System.out.println(recurse(5));
-		System.out.println();
+		Fibonacci obj = new Fibonacci();
 		
-		System.out.println("Iterative");
-		System.out.println(iterate(9));
-		System.out.println(iterate(15));
-		System.out.println(iterate(2));
-		System.out.println(iterate(5));
+		obj.Recurtest();
+		
+		obj.Itertest();
 	}
-
 }
