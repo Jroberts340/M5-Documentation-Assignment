e7a0cff8421352de582ad3574582b190c3c7c954
Justin <justinor340@gmail.com>
2022-03-16 20:18:38 -0400

    Final assignment version?

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -28,19 +28,8 @@ public class Fibonacci {
 
 	public void Recurtest() {
 		
-		int[] arr = new int[] {2, 5, 9, 15};
-		long start = System.nanoTime();
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Fibonacci.recurse(arr[i]);
-			System.out.print(arr[i] + ", ");
-		}
-		long end = System.nanoTime();
-		System.out.print("Runtime in NanoSecond: " + (end - start));
-		System.out.println();
-	}
-	
-	public void Itertest() {
-		
+		System.out.println("RECURSION");
+
 		long start1 = System.nanoTime();
 		System.out.print("Input Number: " + 2 +"\tFibonacci Number: " + Fibonacci.recurse(2));
 		long end1 = System.nanoTime();
@@ -55,15 +44,57 @@ public class Fibonacci {
 		System.out.print("\tRuntime in Nanoseconds: " + run2);
 		System.out.println();
 		
-//		int[] arr = new int[] {2, 5, 9, 15};
-//		long start = System.nanoTime();
-//		for (int i = 0; i < arr.length; i++) {
-//			arr[i] = Fibonacci.iterate(arr[i]);
-//			System.out.print(arr[i] + ", ");
-//		}
-//		long end = System.nanoTime();
-//		System.out.print("Runtime in NanoSecond: " + (end - start));
-//		System.out.println();
+		long start3 = System.nanoTime();
+		System.out.print("Input Number: " + 9 +"\tFibonacci Number: " + Fibonacci.recurse(9));
+		long end3 = System.nanoTime();
+		long run3 = end3 - start3;
+		System.out.print("\tRuntime in Nanoseconds: " + run3);
+		System.out.println();
+		
+		long start4 = System.nanoTime();
+		System.out.print("Input Number: " + 15 +"\tFibonacci Number: " + Fibonacci.recurse(15));
+		long end4 = System.nanoTime();
+		long run4 = end4 - start4;
+		System.out.print("\tRuntime in Nanoseconds: " + run4);
+		System.out.println();
+		System.out.println();
+
+		
+	}
+	
+	public void Itertest() {
+		
+		System.out.println("ITERATION");
+
+		long start1 = System.nanoTime();
+		System.out.print("Input Number: " + 2 +"\tFibonacci Number: " + Fibonacci.iterate(2));
+		long end1 = System.nanoTime();
+		long run1 = end1 - start1;
+		System.out.print("\tRuntime in Nanoseconds: " + run1);
+		System.out.println();
+		
+		long start2 = System.nanoTime();
+		System.out.print("Input Number: " + 5 +"\tFibonacci Number: " + Fibonacci.iterate(5));
+		long end2 = System.nanoTime();
+		long run2 = end2 - start2;
+		System.out.print("\tRuntime in Nanoseconds: " + run2);
+		System.out.println();
+		
+		long start3 = System.nanoTime();
+		System.out.print("Input Number: " + 9 +"\tFibonacci Number: " + Fibonacci.iterate(9));
+		long end3 = System.nanoTime();
+		long run3 = end3 - start3;
+		System.out.print("\tRuntime in Nanoseconds: " + run3);
+		System.out.println();
+		
+		long start4 = System.nanoTime();
+		System.out.print("Input Number: " + 15 +"\tFibonacci Number: " + Fibonacci.iterate(15));
+		long end4 = System.nanoTime();
+		long run4 = end4 - start4;
+		System.out.print("\tRuntime in Nanoseconds: " + run4);
+		System.out.println();
+		
+
 	}
 	
 	public static void main(String[] args) 
